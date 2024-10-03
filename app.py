19f783d92637dd60296e0f30bb53ff567c7d1009
matth <c3035164@ncl.ac.uk>
2024-10-03 17:19:52 +0100

    part 2 complete

--- app.py
+++ app.py
@@ -4,17 +4,6 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template('home/index.html')
-@app.route('/registration')
-def registration():
-    return render_template('accounts/registration.html')
-
-@app.route('/login')
-def login():
-    return render_template('accounts/login.html')
-
-@app.route('/account')
-def account():
-    return render_template('accounts/account.html')
 
 @app.route('/create')
 def create():
--- config.py
+++ config.py
@@ -1,3 +1,10 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+# IMPORT BLUEPRINTS
+from accounts.views import accounts_bp
+...
+
+# REGISTER BLUEPRINTS
+app.register_blueprint(accounts_bp)
