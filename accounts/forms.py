9b99132c02b4a6ddd2633ceefd8388badfd9819e
matth <c3035164@ncl.ac.uk>
2024-10-04 18:02:11 +0100

    part 8 complete (yipeeeee)

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, EqualTo
+from wtforms.validators import DataRequired, EqualTo, Length, Regexp
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired()])
@@ -13,7 +13,14 @@ class RegistrationForm(FlaskForm):
     firstname = StringField('First Name', validators=[DataRequired()])
     lastname = StringField('Last Name', validators=[DataRequired()])
     phone = StringField('Phone', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[
+        DataRequired(),
+        Length(min=8, max=15, message="Password must be between 8 and 15 characters."),
+        Regexp(r'(?=.*[A-Z])', message="Password must contain at least one uppercase letter."),
+        Regexp(r'(?=.*[a-z])', message="Password must contain at least one lowercase letter."),
+        Regexp(r'(?=.*\d)', message="Password must contain at least one digit."),
+        Regexp(r'(?=.*\W)', message="Password must contain at least one special character.")
+    ])
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(),
                                                  EqualTo('password', message='Both password fields must be equal!')])
